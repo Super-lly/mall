56d4652e97caa3c8b1523c9656391bbe0f76a902
Super-lly <843182286@qq.com>
2021-08-07 11:28:13 +0800

    main

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,38 +1,43 @@
 <template>
-  <div class="home">
-    <nav-bar class="home-nav">
-      <div slot="center">购物街</div>
-    </nav-bar>
+  <div id="home" class="wrapper">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners" />
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar.vue";
-import {getHomeMultidata} from "network/home.js"
-
+import HomeSwiper from "./childComps/HomeSwiper";
+import NavBar from "components/common/navbar/NavBar";
+import {getHomeMultidata} from "network/home";
 export default {
   name: "Home",
   components: {
+    HomeSwiper,
     NavBar,
   },
-  data(){
+  data() {
     return {
-      banners:[],
-      recommends:[]
-    }
+      banners: [],
+      recommends: [],
+    };
   },
-  create(){
-    getHomeMultidata().then(res =>{
-      this.banners = res.data.banner.list
-      this.recommends = res.data.recommend.list
-    })
-  }
+  created() {
+    getHomeMultidata().then((res) => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
 };
 </script>
 
 <style scoped>
-  .home-nav{
-    background-color: var(--color-tint);
-    color: #fff;
-  }
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
 </style>
