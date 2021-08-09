38512c60198aa4a86a80a9d3cf3c5e8b7c15dc6a
Super-lly <843182286@qq.com>
2021-08-09 10:19:07 +0800

    main

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,6 +2,8 @@
   <div id="home" class="wrapper">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends"/>
+    <feature-view/>
   </div>
 </template>
 
@@ -9,11 +11,15 @@
 import HomeSwiper from "./childComps/HomeSwiper";
 import NavBar from "components/common/navbar/NavBar";
 import {getHomeMultidata} from "network/home";
+import RecommendView from "./childComps/RecommendView.vue"
+import FeatureView from "./childComps/FeatureView.vue"
 export default {
   name: "Home",
   components: {
     HomeSwiper,
     NavBar,
+    RecommendView,
+    FeatureView
   },
   data() {
     return {
@@ -31,6 +37,9 @@ export default {
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
@@ -39,5 +48,9 @@ export default {
   right: 0;
   top: 0;
   z-index: 9;
+  /* position: fixed;
+  top: 0;
+  left: 0;
+  right: 0; */
 }
 </style>
