c0503a700682d792044d0407b111f2ac3c60f5b8
Super-lly <843182286@qq.com>
2021-08-11 11:34:09 +0800

    main

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,108 +2,25 @@
   <div id="home" class="wrapper">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners" />
-    <recommend-view :recommends="recommends"/>
-    <feature-view/>
-    <tab-control :titles="['流行','新款','精选']" class="tab-control"/>
-    <ul>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-      <li>li</li>
-    </ul>
+    <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control :titles="['流行', '新款', '精选']" class="tab-control" />
+    <goods-list :goods="goods['pop'].list"/>
   </div>
 </template>
 
 <script>
 // 子组件
-import HomeSwiper from "./childComps/HomeSwiper"
-import RecommendView from "./childComps/RecommendView.vue"
-import FeatureView from "./childComps/FeatureView.vue"
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView.vue";
+import FeatureView from "./childComps/FeatureView.vue";
 // 公共组件
-import NavBar from "components/common/navbar/NavBar"
-import TabControl from "components/content/tabControl/TabControl"
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList"
 // 方法
-import {getHomeMultidata} from "network/home"
+import { getHomeMultidata, getHomeGoods } from "network/home";
+import GoodsListItem from '../../components/content/goods/GoodsListItem.vue';
 
 export default {
   name: "Home",
@@ -113,25 +30,48 @@ export default {
     FeatureView,
     NavBar,
     TabControl,
-    TabControl
+    GoodsList,
+    GoodsListItem,
   },
   data() {
     return {
       banners: [],
       recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
     };
   },
   created() {
-    getHomeMultidata().then((res) => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    });
+    this.getHomeMultidata();
+    // 2.请求商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        // console.log(res.data.list);
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
-#home{
+#home {
   padding-top: 44px;
 }
 .home-nav {
@@ -147,8 +87,10 @@ export default {
   left: 0;
   right: 0; */
 }
-.tab-control{
+.tab-control {
   position: sticky;
   top: 44px;
+  background-color: #fff;
+  z-index: 9;
 }
 </style>
