7ca0c6f190a775d920d4c001707c3e735dc4613a
Super-lly <843182286@qq.com>
2021-08-13 11:03:47 +0800

    main

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+  export default {
+    name: "Scroll",
+    data() {
+      return {
+        scroll: null,
+      }
+    },
+    mounted() {
+      // 1.创建BScroll对象
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        
+      })
+    },
+  }
+</script>
+
+<style scoped>
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="home" class="wrapper">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners" />
-    <recommend-view :recommends="recommends" />
-    <feature-view />
-    <tab-control :titles="['流行', '新款', '精选']" class="tab-control" />
-    <goods-list :goods="goods['pop'].list"/>
+    <scroll class="content">
+      <home-swiper :banners="banners" />
+      <recommend-view :recommends="recommends" />
+      <feature-view />
+      <tab-control
+        :titles="['流行', '新款', '精选']"
+        class="tab-control"
+        @tabClick="tabClick"
+      />
+      <goods-list :goods="showGoods" class="goods-list" />
+    </scroll>
   </div>
 </template>
 
@@ -17,10 +23,11 @@ import FeatureView from "./childComps/FeatureView.vue";
 // 公共组件
 import NavBar from "components/common/navbar/NavBar";
 import TabControl from "components/content/tabControl/TabControl";
-import GoodsList from "components/content/goods/GoodsList"
+import GoodsList from "components/content/goods/GoodsList";
+import Scroll from 'components/common/scroll/Scroll'
 // 方法
 import { getHomeMultidata, getHomeGoods } from "network/home";
-import GoodsListItem from '../../components/content/goods/GoodsListItem.vue';
+import GoodsListItem from "../../components/content/goods/GoodsListItem.vue";
 
 export default {
   name: "Home",
@@ -32,6 +39,7 @@ export default {
     TabControl,
     GoodsList,
     GoodsListItem,
+    Scroll,
   },
   data() {
     return {
@@ -42,6 +50,7 @@ export default {
         new: { page: 0, list: [] },
         sell: { page: 0, list: [] },
       },
+      currentType: "pop",
     };
   },
   created() {
@@ -51,7 +60,26 @@ export default {
     this.getHomeGoods("new");
     this.getHomeGoods("sell");
   },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
+    },
+  },
   methods: {
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+    // 网络请求相关
     getHomeMultidata() {
       getHomeMultidata().then((res) => {
         this.banners = res.data.banner.list;
@@ -73,6 +101,7 @@ export default {
 <style scoped>
 #home {
   padding-top: 44px;
+  height: 100vh;
 }
 .home-nav {
   background-color: var(--color-tint);
@@ -82,15 +111,18 @@ export default {
   right: 0;
   top: 0;
   z-index: 9;
-  /* position: fixed;
-  top: 0;
-  left: 0;
-  right: 0; */
 }
 .tab-control {
   position: sticky;
   top: 44px;
   background-color: #fff;
   z-index: 9;
+}
+.goods-list {
+  padding-bottom: 50px;
+}
+.content{
+  height: (100vh);
+  /* overflow: hidden; */
 }
 </style>
