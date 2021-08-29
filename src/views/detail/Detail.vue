ad51fc0a5f9dce203c3b66b6f460c6f2effd1cf4
Super-lly <843182286@qq.com>
2021-08-29 09:25:19 +0800

    main

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,45 +1,75 @@
 <template>
-  <div>
+  <div id="detail">
     <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages" />
-    <detail-base-info :goods="goods"/>
-    <detail-shop-info :shop="shop"/>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages" />
+      <detail-base-info :goods="goods" />
+      <detail-shop-info :shop="shop" />
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
+    </scroll>
   </div>
 </template>
 
 <script>
+import DetailNavBar from "./childComps/DetailNavBar.vue";
+import DetailSwiper from "./childComps/DetailSwiper.vue";
+import DetailBaseInfo from "./childComps/DetailBaseInfo.vue";
+import DetailShopInfo from "./childComps/DetailShopInfo.vue";
+import DetailGoodsInfo from "./childComps/DetailGoodsInfo.vue";
 
-import DetailNavBar from "./childComps/DetailNavBar.vue"
-import DetailSwiper from "./childComps/DetailSwiper.vue"
-import DetailBaseInfo from "./childComps/DetailBaseInfo.vue"
-import DetailShopInfo from "./childComps/DetailShopInfo.vue"
+import Scroll from "components/common/scroll/Scroll";
 
-import {getDetail, Goods, Shop} from "network/detail"
+import { getDetail, Goods, Shop } from "network/detail";
 
 export default {
-  name : 'Detail',
-  components:{
+  name: "Detail",
+  components: {
     DetailNavBar,
     DetailSwiper,
     DetailBaseInfo,
-    DetailShopInfo
+    DetailShopInfo,
+    DetailGoodsInfo,
+    Scroll,
   },
-  data(){
-    return{
-      iid:null,
-      topImages:[],
-      goods:{},
-      shop:{}
+  data() {
+    return {
+      iid: null,
+      topImages: [],
+      goods: {},
+      shop: {},
+      detailInfo:{}
+    };
+  },
+  created() {
+    this.iid = this.$route.params.iid;
+    getDetail(this.iid).then((res) => {
+      const data = res.result;
+      this.topImages = data.itemInfo.topImages;
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      );
+      this.shop = new Shop(data.shopInfo);
+      this.detailInfo = data.detailInfo
+    });
+  },
+  methods:{
+    imageLoad(){
+      this.$refs.scroll.refresh()
     }
-  },
-  created(){
-    this.iid = this.$route.params.iid
-    getDetail(this.iid).then(res=>{
-      const data = res.result
-      this.topImages = data.itemInfo.topImages
-      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
-      this.shop = new Shop(data.shopInfo)
-    })
   }
-}
+};
 </script>
+<style scoped>
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+.content{
+  height: calc(100% - 44px);
+  overflow: hidden;
+}
+</style>
